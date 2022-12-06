4f7289ab3f271475d5fdefb0f21e4f8ffec221e8
roosterMAP <lead.acrysoft@gmail.com>
2022-12-06 14:35:59 -0800

    bug fix
    fixed bug with thicken op. it can now handle multiple pairs of open edge loops.

--- addon/thicken.py
+++ addon/thicken.py
@@ -4,37 +4,53 @@ import rmKit.rmlib as rmlib
 import mathutils
 
 def BridgeSurfaces( bm, faces1, faces2 ):
-	#create edge loops from two poly surfaces
-	loops = []
-	loops.append( rmlib.rmEdgeSet( [ e for e in faces1.edges if e.is_boundary ] ).vert_chain()[0] )
-	loops.append( rmlib.rmEdgeSet( [ e for e in faces2.edges if e.is_boundary ] ).vert_chain()[0] )
-	
-	#check if loop1 is sorted based of a neigh face
-	for i in range( 2 ):
-		epts = ( loops[i][0], loops[i][1] )
-		edge = rmlib.rmEdgeSet.from_endpoints( epts[0], epts[1] )
-		face_verts = list( edge.link_faces[0].verts )
-		vert_idx = face_verts.index( epts[1] )
-		if face_verts[vert_idx-1] == epts[0]:
-			loops[i].reverse()
-	
-	#align loop1 and loop2
-	sample_pos = loops[0][0].co.copy()
-	min_len = 9999999.9
-	min_idx = -1
-	for i, v in enumerate( loops[1] ):
-		d = ( v.co - sample_pos ).length
-		if d < min_len:
-			min_len = d
-			min_idx = i
-		
-	#bridge the loops
 	new_faces = rmlib.rmPolygonSet( [] )
-	vcount = len( loops[0] )
-	for i in range( vcount ):
-		j = ( min_idx + i ) % vcount
-		quad = ( loops[0][i-1], loops[0][i], loops[1][j], loops[1][j-1] )
-		new_faces.append( bm.faces.new( quad, faces1[0] ) )
+
+	#create edge loops from two poly surfaces
+	loops_a = rmlib.rmEdgeSet( [ e for e in faces1.edges if e.is_boundary ] ).vert_chain()
+	loops_b = rmlib.rmEdgeSet( [ e for e in faces2.edges if e.is_boundary ] ).vert_chain()
+
+	for l_a in loops_a:
+		p1 = mathutils.Vector( l_a[0].co.copy() )
+		nearest_loop = None
+		nearest_dist = 99999.9
+		for l_b in loops_b:
+			for v in l_b:
+				p2 = mathutils.Vector( v.co.copy() )
+				dist = ( p2 - p1 ).length
+				if dist < nearest_dist:
+					nearest_dist = dist
+					nearest_loop = l_b
+		if nearest_loop is None:
+			continue
+
+		loops = [ l_a, nearest_loop ]
+	
+		#check if loop1 is sorted based of a neigh face
+		for i in range( 2 ):
+			epts = ( loops[i][0], loops[i][1] )
+			edge = rmlib.rmEdgeSet.from_endpoints( epts[0], epts[1] )
+			face_verts = list( edge.link_faces[0].verts )
+			vert_idx = face_verts.index( epts[1] )
+			if face_verts[vert_idx-1] == epts[0]:
+				loops[i].reverse()
+		
+		#align loop1 and loop2
+		sample_pos = loops[0][0].co.copy()
+		min_len = 9999999.9
+		min_idx = -1
+		for i, v in enumerate( loops[1] ):
+			d = ( v.co - sample_pos ).length
+			if d < min_len:
+				min_len = d
+				min_idx = i
+			
+		#bridge the loops
+		vcount = len( loops[0] )
+		for i in range( vcount ):
+			j = ( min_idx + i ) % vcount
+			quad = ( loops[0][i-1], loops[0][i], loops[1][j], loops[1][j-1] )
+			new_faces.append( bm.faces.new( quad, faces1[0] ) )
 
 	return new_faces
 
