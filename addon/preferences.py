2d298212b778509baafb5c161c226d631f530ada
roosterMAP <lead.acrysoft@gmail.com>
2022-12-20 07:02:44 -0800

    prefs work
    more prefs work

--- addon/preferences.py
+++ addon/preferences.py
@@ -1,58 +1,157 @@
 import bpy, rna_keymap_ui
 
-UTILS_KEYMAP = []
-SELECTION_KEYMAP = []
+RM_KEYMAP = []
 
 #https://docs.blender.org/api/current/bpy.types.KeyMapItems.html#bpy.types.KeyMapItems
 
 def register_keyboard_keymap():
 	kc = bpy.context.window_manager.keyconfigs.addon
 	if kc:
-		km = kc.keymaps.new( name='Mesh', space_type='EMPTY' )
-		UTILS_KEYMAP.append( ( km, km.keymap_items.new( 'mesh.rm_copy', 'NONE', 'PRESS' ) ) )
-		UTILS_KEYMAP.append( ( km, km.keymap_items.new( 'mesh.rm_paste', 'NONE', 'PRESS' ) ) )
-		UTILS_KEYMAP.append( ( km, km.keymap_items.new( 'view3d.rm_togglegrid', 'NONE', 'PRESS' ) ) )
-		UTILS_KEYMAP.append( ( km, km.keymap_items.new( 'view3d.rm_workplane', 'NONE', 'PRESS' ) ) )
-		UTILS_KEYMAP.append( ( km, km.keymap_items.new( 'view3d.rm_cursor_to_selection', 'NONE', 'PRESS' ) ) )
-		UTILS_KEYMAP.append( ( km, km.keymap_items.new( 'view3d.rm_unrotate_relative_to_cursor', 'NONE', 'PRESS' ) ) )
-		UTILS_KEYMAP.append( ( km, km.keymap_items.new( 'view3d.rm_origin_to_cursor', 'NONE', 'PRESS' ) ) )
+		km_3dview = kc.keymaps.new( name='3D View', space_type='EMPTY' )
+		km_mesh = kc.keymaps.new( name='Mesh', space_type='EMPTY' )
+		km_uv = kc.keymaps.new( name='UV Editor', space_type='EMPTY' )
+
+		RM_KEYMAP.append( ( km_3dview, km_3dview.keymap_items.new( 'view3d.rm_togglegrid', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_3dview, km_3dview.keymap_items.new( 'view3d.rm_workplane', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_3dview, km_3dview.keymap_items.new( 'view3d.rm_cursor_to_selection', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_3dview, km_3dview.keymap_items.new( 'view3d.rm_unrotate_relative_to_cursor', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_3dview, km_3dview.keymap_items.new( 'view3d.rm_origin_to_cursor', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_arcadjust', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_connectedge', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_contextbevel', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_createtube', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_extend', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_grabapplymat', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_quickmaterial', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'messh.rm_radialalign', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.grabapplyuvbounds', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_uv, km_uv.keymap_items.new( 'mesh.moshotspot', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_uv, km_uv.keymap_items.new( 'mesh.nrsthotspot', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_uv, km_uv.keymap_items.new( 'mesh.matchhotspot', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_loop', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_ring', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_uv, km_uv.keymap_items.new( 'mesh.rm_uvloop', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_uv, km_uv.keymap_items.new( 'mesh.rm_uvring', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_polypatch', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_uv, km_uv.keymap_items.new( 'mesh.rm_uvrectangularize', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_uv, km_uv.keymap_items.new( 'mesh.rm_uvgridify', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_remove', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_uv, km_uv.keymap_items.new( 'mesh.rm_relativeislands', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_invertcontinuous', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_uv, km_uv.keymap_items.new( 'mesh.rm_stitch', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_targetweld', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_mesh, km_mesh.keymap_items.new( 'mesh.rm_thicken', 'NONE', 'PRESS' ) ) )
+		RM_KEYMAP.append( ( km_uv, km_uv.keymap_items.new( 'mesh.rm_uvunrotate', 'NONE', 'PRESS' ) ) )
+
+		kmi = km_mesh.keymap_items.new( 'mesh.rm_copy', 'NONE', 'PRESS' )
+		kmi.properties.cut = True
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'mesh.rm_copy', 'NONE', 'PRESS' )
+		kmi.properties.cut = False
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'mesh.rm_paste', 'NONE', 'PRESS' )
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'mesh.rm_changemodeto', 'NONE', 'PRESS' )
+		kmi.properties.mode_to = 'VERT'
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'mesh.rm_changemodeto', 'NONE', 'PRESS' )
+		kmi.properties.mode_to = 'EDGE'
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'mesh.rm_changemodeto', 'NONE', 'PRESS' )
+		kmi.properties.mode_to = 'FACE'
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'mesh.rm_convertmodeto', 'NONE', 'PRESS' )
+		kmi.properties.mode_to = 'VERT'
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'mesh.rm_convertmodeto', 'NONE', 'PRESS' )
+		kmi.properties.mode_to = 'EDGE'
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'mesh.rm_convertmodeto', 'NONE', 'PRESS' )
+		kmi.properties.mode_to = 'FACE'
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'mesh.rm_continuous', 'NONE', 'PRESS' )
+		kmi.properties.add = False
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'mesh.rm_continuous', 'NONE', 'PRESS' )
+		kmi.properties.add = True
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'wm.call_menu_pie', 'NONE', 'PRESS' )
+		kmi.properties.name = 'VIEW3D_MT_PIE_movetofurthest'
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'wm.call_menu_pie', 'NONE', 'PRESS' )
+		kmi.properties.name = 'VIEW3D_MT_PIE_cursor'
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'wm.call_menu_pie', 'NONE', 'PRESS' )
+		kmi.properties.name = 'VIEW3D_MT_PIE_setedgeweight_crease'
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'wm.call_menu', 'NONE', 'PRESS' )
+		kmi.properties.name = 'OBJECT_MT_rm_knifescreen'
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_uv.keymap_items.new( 'wm.call_menu_pie', 'NONE', 'PRESS' )
+		kmi.properties.name = 'IMAGE_EDITOR_MT_PIE_uvmovetofurthest'
+		RM_KEYMAP.append( ( km_uv, kmi ) )
+
+		kmi = km_mesh.keymap_items.new( 'wm.call_menu_pie', 'NONE', 'PRESS' )
+		kmi.properties.name = 'OBJECT_MT_rm_screenreflect'
+		RM_KEYMAP.append( ( km_mesh, kmi ) )
+
+		kmi = km_3dview.keymap_items.new( 'wm.call_menu_pie', 'NONE', 'PRESS' )
+		kmi.properties.name = 'VIEW3D_MT_PIE_cursor'
+		RM_KEYMAP.append( ( km_3dview, kmi ) )
 
 
 def unregister_keyboard_keymap():
-	for km, kmi in UTILS_KEYMAP:
+	for km, kmi in RM_KEYMAP:
 		km.keymap_items.remove( kmi )
-	UTILS_KEYMAP.clear()
-
-	for km, kmi in SELECTION_KEYMAP:
-		km.keymap_items.remove( kmi )
-	SELECTION_KEYMAP.clear()
+	RM_KEYMAP.clear()
 
 
 class RMKITPreferences( bpy.types.AddonPreferences ):
 	bl_idname = 'rmKit'
 
-	utils_checkbox: bpy.props.BoolProperty( name="Utilities", default=True )
-	sel_checkbox: bpy.props.BoolProperty( name="Selection", default=True )
+	v3d_checkbox: bpy.props.BoolProperty( name="3D View", default=False )
+	mesh_checkbox: bpy.props.BoolProperty( name="Mesh", default=False )
+	uv_checkbox: bpy.props.BoolProperty( name="UV Editor", default=False )
 
 	def draw( self, context ):
 		layout = self.layout
 		box = layout.box()
-		
-		row_utils = box.row()
-		row_utils.prop( self, 'utils_checkbox', icon='TRIA_DOWN' if self.utils_checkbox else 'TRIA_RIGHT', icon_only=True, emboss=False )
-		row_utils.label( text='Utilities' )
-		if self.utils_checkbox:
-			col = box.column( align=True )
-			self.draw_keymap_items( col, 'Mesh', UTILS_KEYMAP, {'KEYBOARD'}, False )
 
-		'''
-		sel_utils = box.row()
-		sel_utils.prop( self, 'sel_checkbox', icon='TRIA_DOWN' if self.sel_checkbox else 'TRIA_RIGHT', icon_only=True, emboss=False )
-		sel_utils.label( text='Selection' )
-		if self.sel_checkbox:
+		row_view3d = box.row()
+		row_view3d.prop( self, 'v3d_checkbox', icon='TRIA_DOWN' if self.v3d_checkbox else 'TRIA_RIGHT', icon_only=True, emboss=False )
+		row_view3d.label( text='3D View' )
+		if self.v3d_checkbox:
 			col = box.column( align=True )
-			self.draw_keymap_items( col, 'Mesh', SELECTION_KEYMAP, {'KEYBOARD'}, False )
-		'''
+			self.draw_keymap_items( col, '3D View', RM_KEYMAP, {'KEYBOARD'}, False )
+
+		row_mesh = box.row()
+		row_mesh.prop( self, 'mesh_checkbox', icon='TRIA_DOWN' if self.mesh_checkbox else 'TRIA_RIGHT', icon_only=True, emboss=False )
+		row_mesh.label( text='Mesh' )
+		if self.mesh_checkbox:
+			col = box.column( align=True )
+			self.draw_keymap_items( col, 'Mesh', RM_KEYMAP, {'KEYBOARD'}, False )
+
+		row_uv = box.row()
+		row_uv.prop( self, 'uv_checkbox', icon='TRIA_DOWN' if self.uv_checkbox else 'TRIA_RIGHT', icon_only=True, emboss=False )
+		row_uv.label( text='UV Editor' )
+		if self.uv_checkbox:
+			col = box.column( align=True )
+			self.draw_keymap_items( col, 'UV Editor', RM_KEYMAP, {'KEYBOARD'}, False )
 
 	@staticmethod
 	def draw_keymap_items( col, km_name, keymap, map_type, allow_remove=False ):
@@ -64,7 +163,7 @@ class RMKITPreferences( bpy.types.AddonPreferences ):
 
 		kmis = [ kmi for kmi in km.keymap_items if kmi.idname in kmi_idnames and kmi.map_type in map_type ]
 		for kmi in kmis:
-			rna_keymap_ui.draw_kmi( ['ADDON'], kc, km, kmi, col, 0 )
+			rna_keymap_ui.draw_kmi( [], kc, km, kmi, col, 0 )
 		
 
 def register():
